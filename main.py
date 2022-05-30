1a7bf7b5d6362bb0444df13d094d78ba4bb11d64
GibNub <87067148+GibNub@users.noreply.github.com>
2022-05-31 11:41:07 +1200

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ from markupsafe import Markup
 app = Flask(__name__)
 
 
-# This is the html template for indivdual forum posts
-# this code may need to be replaced with a javascript method
+# This is the html template for indivdual forum posts.
 def forum_post(title, type, date, content, like, dislike, id):
     html_template = """
     <li class="thread">
@@ -39,6 +38,8 @@ def forum_post(title, type, date, content, like, dislike, id):
     """.format(title, type, date, content, like, dislike, id)
     return html_template
 
+
+# Same as above.
 def comment_and_reply(user_id, content, like, dislike, date):
     comment_template = """
     <li class="comment">
@@ -58,19 +59,20 @@ def comment_and_reply(user_id, content, like, dislike, date):
             <p>
                 {4}
             </p>
+            <div class="replies">
+                {{ reply }}
+            </div>
     """.format(user_id, content, like, dislike, date)
 
 
-
-
-
-# Gather the information for forum posts from the database
+# Gather the information for forum posts from the database.
 def call_page_database(i):
     conn = sqlite3.connect("forum_database.db")
     cur = conn.cursor()
     cur.execute("SELECT * FROM Post WHERE id = ?", (str(i),))
     return cur.fetchone()
     conn.close()
+
 
 def call_comment_database(i):
     conn = sqlite3.connect("forum_database.db")
@@ -92,23 +94,23 @@ def update_post(type, title, content):
 
 # Templates the html forum index with information from the database.
 # Then joins them in one string to then be passed to the actual html file.
-# This creates the expandable forum
+# This creates the expandable forum.
 @app.route("/")
 def home():
-    # Create the limited information posts on the front page
+    # Create the posts on the front page by looping through the database.
     forum_html = []
     i = 1
     while True:
         if call_page_database(i) is None:
             break
         forum_html.append(forum_post(
-            call_page_database(i)[2],  # title
-            call_page_database(i)[1],  # type
-            call_page_database(i)[3],  # content
-            call_page_database(i)[4],  # like
-            call_page_database(i)[5],  # dislike
-            call_page_database(i)[7],  # date
-            call_page_database(i)[0],  # id
+            call_page_database(i)[2],  # title.
+            call_page_database(i)[1],  # type.
+            call_page_database(i)[3],  # content.
+            call_page_database(i)[4],  # like.
+            call_page_database(i)[5],  # dislike.
+            call_page_database(i)[7],  # date.
+            call_page_database(i)[0],  # id.
         ))
         i += 1
     forum_html = "".join(forum_html)
@@ -118,6 +120,9 @@ def home():
     return render_template("home.html", value=forum_html)
 
 
+# This gets the form values from the home page,
+# The variables get updated to the database.
+# Then the home page then gets refreshed.
 @app.route("/create_post", methods=["GET", "POST"])
 def create_post():
     if request.method == "POST":
@@ -128,24 +133,28 @@ def create_post():
         return redirect(url_for("home"), code=302)
 
 
+# Directs user to the about page
 @app.route("/about")
 def about():
     return render_template("about.html")
 
 
+# The route creates pages automatically for each pages.
+# The id variable passed into the call database function.
 @app.route("/pages/<int:id>")
 def page(id):
     return render_template("page.html",
-                           title=call_page_database(id)[2], # title
-                           type=call_page_database(id)[1], # type
-                           content=call_page_database(id)[3], # content
-                           date=call_page_database(id)[7], # date
-                           user_id=call_page_database(id)[6], # user id
-                           like=call_page_database(id)[4], # like
-                           dislike=call_page_database(id)[5], # dislike
+                           title=call_page_database(id)[2],  # title.
+                           type=call_page_database(id)[1],  # type.
+                           content=call_page_database(id)[3],  # content.
+                           date=call_page_database(id)[7],  # date.
+                           user_id=call_page_database(id)[6],  # user id.
+                           like=call_page_database(id)[4],  # like.
+                           dislike=call_page_database(id)[5],  # dislike.
                            )
 
 
+# A page exclusively to search for specific posts.
 @app.route("/search")
 def search():
     return render_template()
@@ -153,6 +162,3 @@ def search():
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port="8000")
-
-
-# advanced search
