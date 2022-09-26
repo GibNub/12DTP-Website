6a8444e344d12d8ade3fac1f519e054359d76a69
GibNub <87067148+GibNub@users.noreply.github.com>
2022-09-27 12:41:38 +1300

    About page, body margin

--- main.py
+++ main.py
@@ -675,4 +675,4 @@ def teardown_db(_):
 
 if __name__ == "__main__":
     CSRFProtect().init_app(app)
-    app.run(debug=True, host="0.0.0.0", port="8000")
+    app.run(debug=False, host="0.0.0.0", port="8000")
